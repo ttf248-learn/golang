eb4ca5d6577163b6940c15930c0750921d64965a
向天龙 <tianlong.xiang@longbridge.sg>
2021-05-07 20:03:26 +0800

    Go 运算符号: << >> * &

--- cmd/tmp.go
+++ cmd/tmp.go
@@ -20,11 +20,22 @@ func main() {
 
 	const conStr string = "const string"
 	const conStr2 = "const string 2"
-
 	fmt.Println(conStr, conStr2, Unknown, Female, Male, One, Two)
 
-	_, b, c := multi_()
+	/* << 左移运算符，相当于乘以2的n次方 >> 右移运算符，相当于除以2的n次方 */
 
+	var tmp1 int = 60
+	fmt.Println(tmp1<<1, tmp1>>1)
+
+	tmp2 := 30
+	var tmp3 *int
+	fmt.Println("tmp2:", tmp2, "tmp3:", tmp3)
+	tmp3 = &tmp2
+	fmt.Println("tmp2:", tmp2, "tmp3:", *tmp3)
+	tmp2 = 60
+	fmt.Println("tmp2:", tmp2, "tmp3:", *tmp3)
+
+	_, b, c := multi_()
 	fmt.Println(b, c)
 
 	if len(os.Args) > 1 {
