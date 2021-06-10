810161fa56a0283efcfb34cb6ddbc7dacbf9ce5f
向天龙 <tianlong.xiang@longbridge.sg>
2021-06-10 17:38:45 +0800

    chron: 添加客户端调用时间统计

--- cmd/ch5_go_micro_client.go
+++ cmd/ch5_go_micro_client.go
@@ -14,6 +14,19 @@ import (
 	message "go-base-learning/endpoint"
 )
 
+type BenchTime struct {
+	name  string
+	value time.Duration
+}
+
+func autoAddIndex() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
 func main() {
 	consulRegistry := consul.NewRegistry(registry.Addrs(global.GetConsulInfo()))
 
@@ -21,11 +34,18 @@ func main() {
 		micro.Name("student.client"),
 		micro.Registry(consulRegistry))
 
+	var bench [100]BenchTime
+	index := autoAddIndex()
+	now := time.Now()
 	svc.Init()
+	bench[index()] = BenchTime{"init server", time.Since(now)}
 
 	studentService := message.NewStudentService("lb.student.endpoint", svc.Client())
+	bench[index()] = BenchTime{"new client", time.Since(now)}
 
 	res, err := studentService.GetStudent(context.TODO(), &message.StudentRequest{Name: "davie"})
+	bench[index()] = BenchTime{"call server", time.Since(now)}
+
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -34,5 +54,11 @@ func main() {
 	fmt.Println(res.Name)
 	fmt.Println(res.Classes)
 	fmt.Println(res.Grade)
-	time.Sleep(50 * time.Second)
+	//time.Sleep(50 * time.Second)
+
+	for index, item := range bench {
+		if item.value != 0 {
+			fmt.Println(index, item.name, item.value.Microseconds())
+		}
+	}
 }
