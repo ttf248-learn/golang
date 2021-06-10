b6c269fa8e0d91fa9768e2581c8b0d7f51422034
向天龙 <tianlong.xiang@longbridge.sg>
2021-06-10 17:04:33 +0800

    chron: 默认连接腾讯云服务器

--- global/global.go
+++ global/global.go
@@ -8,10 +8,10 @@ import (
 func GetConsulInfo() string {
 	var ret string
 
-	if os.Args[1] == "remote" {
-		ret = "134.175.47.100:8500"
-	} else {
+	if len(os.Args) > 1 && os.Args[1] == "local" {
 		ret = "localhost:8500"
+	} else {
+		ret = "134.175.47.100:8500"
 	}
 	fmt.Println("GetConsulInfo:", ret)
 	return ret
