6bd41da5f5f5746eebd08dc2606fed576c8da274
向天龙(技术中心/境外柜台研发部) <tianlong.xiang@yintech.cn>
2023-02-09 15:02:09 +0800

    更新 gorm 版本、新增 update 性能测试函数

--- cmd/ch9_gorm_mysql.go
+++ cmd/ch9_gorm_mysql.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"go-base-learning/data_provider/dao"
 	"strconv"
+	"time"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/shopspring/decimal"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func AutoString() func() string {
@@ -28,18 +29,25 @@ func AutoNumber() func() decimal.Decimal {
 }
 
 func main() {
-	db, err := gorm.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local")
+	dsn := "root:sErjf&eNh9zv4CSV@tcp(192.168.19.146:5306)/ykcz_trade?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-	db.SingularTable(true) // 禁用默认表名的复数形式，如果置为 true，则 `User` 的默认表名是 `user`
+	// defer db.Close()
+
+	// db.SingularTable(true) // 禁用默认表名的复数形式，如果置为 true，则 `User` 的默认表名是 `user`
 
 	db.AutoMigrate(&dao.MessagesIBDropCopy{})
 
+	// 两个自增函数对象
 	tmpString := AutoString()
 	tmpNumber := AutoNumber()
-	for i := 0; i < 10000; i++ {
+
+	for i := 0; i < 1; i++ {
 		idxMessageIBDropCopy := dao.MessagesIBDropCopy{
 			F11_ClOrdID:           tmpString(),
 			F41_OrigClOrdID:       tmpString(),
@@ -67,6 +75,28 @@ func main() {
 
 		if err := db.Create(&idxMessageIBDropCopy).Error; err != nil {
 			panic(err)
+		}
+	}
+
+	// 查询数据是否存在
+	record := dao.MessagesIBDropCopy{}
+	db.First(&record)
+
+	var count int64
+	count = 0
+
+	bT := time.Now() // 开始时间
+
+	for i := 0; i < 100000; i++ {
+		if err := db.Model(&record).Select("F11_ClOrdID").Update("F11_ClOrdID", tmpString()).Error; err != nil {
+			panic(err)
+		} else {
+			count++
+		}
+
+		if count%1000 == 0 {
+			eT := time.Since(bT).Microseconds() // 从开始到当前所消耗的时间
+			fmt.Println("call ", count, " times, avg cost time(us): ", eT, " tps: ", 1000000/(eT/count))
 		}
 	}
 
